4901b6fdff239741127dae0ab1b315ca7f9e20dd
J-CodeV <j.codev.pro@gmail.com>
2025-01-27 22:59:57 +0100

    ebauche de liste pour les table

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>bienvenue</h1>
-    </div>
-  );
+  //state
+  const [checked, setChecked] = useState(true);
 
-  <button onClick={reset} type="button">
-    suppr
-  </button>;
+  // comportements
+  const toggelCheck = () => {
+    setChecked(!checked);
+  };
+
+  // render ( affichage)
+  return (
+    <form>
+      <h1>COMMANDE</h1>
+      <p>
+        Valider la prise de commande
+        <input type="checkbox" checked={checked} onChange={toggelCheck} />
+        <h2>terrasse</h2>
+      </p>
+      {checked && <button>table 1</button>}
+      {checked && <button>table 2</button>}
+      {checked && <button>table 3</button>}
+
+      <h2>salle principal</h2>
+      {checked && <button>table 4</button>}
+      {checked && <button>table 5</button>}
+      {checked && <button>table 6</button>}
+      {checked && <button>table 7</button>}
+      {checked && <button>table 8</button>}
+      <h2>salle coin cuisine</h2>
+      {checked && <button>table 9</button>}
+      {checked && <button>table 10</button>}
+      {checked && <button>table 11</button>}
+      {checked && <button>table 12</button>}
+      {checked && <button>table 13</button>}
+    </form>
+  );
+  // A FAIRE UN PLAN VUE DE HAUT DES TABLE DU RESTAURANT QUI SILUMIBE AU ONCLICK
+  // <button onClick={reset} type="button">
+  //   suppr
+  // </button>  "btn de reset"
 }
 
 export default App;
